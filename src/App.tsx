ba7a387b391eefa12c1e039fbb8f4fb54f988982
Insafsuz <in7afsuz@gmail.com>
2024-06-19 11:35:00 +0300

    created ui and header,footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Modal from './components/ui/Modal/Modal'
 
 const App: FC = () => {
-  return <></>
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  return (
+    <div className='wrapper'>
+      <Header />
+      <main className='page'></main>
+      <Footer />
+      <Modal
+        isModalVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
+      >
+        <img src='images/image-rules.svg' alt='' />
+      </Modal>
+    </div>
+  )
 }
 
 export default App
